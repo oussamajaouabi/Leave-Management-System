a47bd78c5a6cf6fa671a0c0af6320fbc9b12325b
Oussama Jaouabi <96437883+oussamajaouabi@users.noreply.github.com>
2025-12-24 17:33:31 +0100

    Update database credentials in DatabaseConfig

--- Projet _ GESTION_DE_CONGES/src/utilities/DatabaseConfig.java
+++ Projet _ GESTION_DE_CONGES/src/utilities/DatabaseConfig.java
@@ -2,6 +2,6 @@ package utilities;
 
 public interface DatabaseConfig {
 	String URL = "jdbc:mysql://localhost:3307/gestion_conges_db";
-    String USER = "root";
-    String PASSWORD = "root";
+    String USER = "votre_utilisateur";
+    String PASSWORD = "votre_mot_de_passe";
 }
